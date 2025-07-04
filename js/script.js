c824720a1204274e4db7ed106d4792c6c128a162
Carlos Daniel <danielmouraaguiar@gmail.com>
2025-07-03 23:18:19 -0300

    Finalizado

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
-document.getElementById('incrementar').addEventListener('click', 
+document.getElementById('incrementar').addEventListener('click', more1)
     
-    function() {
+    function more1() {
 
-    const input = document.getElementById('contador');
-    let valorAtual = Number(input.value) || 0;
-    input.value = valorAtual + 1;
-});
+        const input = document.getElementById('contador');
+        let valorAtual = Number(input.value) || 0;
+        input.value = valorAtual + 1;
+    };
+
+    document.getElementById('less').addEventListener('click', less1);
+
+    function less1() {
+
+        const input = document.getElementById('contador');
+        let valorAtual = Number(input.value) || 0;
+        input.value = valorAtual - 1;
+    };
+
+    document.getElementById('reset').addEventListener('click', reset);
+
+    function reset() {
+
+        const input = document.getElementById('contador');
+        input.value = 0;
+    };
